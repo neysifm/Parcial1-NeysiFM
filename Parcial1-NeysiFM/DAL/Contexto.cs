7b91e95ed0c9a1ef673f76063bb9faab4c7f5fdd
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-05-30 16:08:29 -0400

    Programando la DAL

--- Parcial1-NeysiFM/DAL/Contexto.cs
+++ Parcial1-NeysiFM/DAL/Contexto.cs
@@ -11,7 +11,7 @@ namespace Parcial1_NeysiFM.DAL
 {
     public class Contexto : DbContext
     {
-        public DbSet<Productos> Producto { get; set; }
+        public DbSet <Productos> Producto { get; set; }
 
         public Contexto() : base("ConStr") { }
     }
