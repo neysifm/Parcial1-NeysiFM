6ed1363b9e311932aa50fc825f56b25094f8dac8
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-05-31 10:51:03 -0400

    Estructura de Apliacada 1

---
+++ Parcial1-NeysiFM/UI/Reportes/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_NeysiFM.UI.Reportes
+{
+    class Class1
+    {
+    }
+}
